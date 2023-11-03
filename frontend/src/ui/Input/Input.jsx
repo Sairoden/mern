55ff2f366eef34c91f652fe280116be9715f32d4
Sairoden <odingandarosa@gmail.com>
2023-11-03 17:13:51 +0800

    Manage input state

--- frontend/src/ui/Input/Input.jsx
+++ frontend/src/ui/Input/Input.jsx
@@ -1,18 +1,57 @@
+// React & Libraries
+import { useReducer } from "react";
+
 // Styles
 import "./Input.css";
 
+const inputReducer = (state, action) => {
+  switch (action.type) {
+    case "CHANGE":
+      return { ...state, value: action.payload, isValid: true };
+
+    default:
+      return state;
+  }
+};
+
+const INITIAL_STATE = {
+  value: "",
+  isValid: false,
+};
+
 function Input(props, { id, rows, placeholder, type }) {
+  const [{ value, isValid }, dispatch] = useReducer(
+    inputReducer,
+    INITIAL_STATE
+  );
+
+  const changeHandler = e => {
+    dispatch({ type: "CHANGE", payload: e.target.value });
+  };
+
   const element =
     props.element === "input" ? (
-      <input id={id} type={type} placeholder={placeholder} />
+      <input
+        id={id}
+        type={type}
+        placeholder={placeholder}
+        onChange={changeHandler}
+        value={value}
+      />
     ) : (
-      <textarea id={id} rows={rows || 3} />
+      <textarea
+        id={id}
+        rows={rows || 3}
+        onChange={changeHandler}
+        value={value}
+      />
     );
 
   return (
-    <div className={`form-control`}>
+    <div className={`form-control ${!isValid && "form-control--invalid"}`}>
       <label htmlFor={id}>{props.label}</label>
       {element}
+      {!isValid && <p>{props.errorText}</p>}
     </div>
   );
 }
--- frontend/src/ui/PlaceForm/PlaceForm.jsx
+++ frontend/src/ui/PlaceForm/PlaceForm.jsx
@@ -7,7 +7,13 @@ import { Input } from "../index";
 function PlaceForm() {
   return (
     <form className="place-form">
-      <Input type="text" label="Title" element="input" />
+      <Input
+        type="text"
+        label="Title"
+        element="input"
+        validators={[]}
+        errorText="Please enter a valid title"
+      />
     </form>
   );
 }
